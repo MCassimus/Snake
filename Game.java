ba3f1a10b4bc3ac6d63477d4edb1e0f3ef2babf8
Michael Cassimus <18mac03@stu.cpsk12.org>
2017-05-04 11:36:23 -0500

    Start to Project

---
+++ Game.java
@@ -0,0 +1,12 @@
+public class Game
+{
+    public Game()
+    {
+        Snake snake = new Snake();
+        
+        while(true)
+        {
+            
+        }
+    }
+}
---
+++ Window.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Window extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.fillRect(0, 0, 800, 600);//set the background to black
+    }
+}
